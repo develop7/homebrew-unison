4bd076a70924f3caedf97a2d9522eefee6171665
Arya Irani <arya.irani@gmail.com>
2019-08-06 13:20:53 -0400

    update to release/azure40

--- unison-language.rb
+++ unison-language.rb
@@ -2,14 +2,14 @@ class UnisonLanguage < Formula
   desc "The Unison Language and Codebase Manager."
   homepage "https://unisonweb.org"
 
-  version "1.0.M1a"
+  version "1.0.azure40"
 
   if OS.mac?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM1a/unison-osx.tar.gz"
-    sha256 "a1fd7210b02a4d389bb89d39538767b70cb5ddb1af5045b876c315fa87d1e6db"
+    url "https://github.com/unisonweb/unison/releases/download/release%2Fazure40/unison-osx.tar.gz"
+    sha256 "62836f30f4ed6513e909b49eb1ddd225deb22fe7052dace375de2084a0a2566b"
   elsif OS.linux?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM1a/unison-linux64.tar.gz"
-    sha256 "4ba52595d2d89772327c689f08728e5f19499f3181b712b8708e71da2fccf4e6"
+    url "https://github.com/unisonweb/unison/releases/download/release%2Fazure40/unison-linux64.tar.gz"
+    sha256 "b04c34bef09e532f7374446cd2977df51e85f522d5e9f2fdcfa687965a7a10c4"
   end
 
   def install
